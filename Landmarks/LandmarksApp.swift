8d436aa2ae8f2bfd24a80933fe5bd5b4e9606d9b
Vladyslav Burdeniuk <vladyslavb@spotify.com>
2024-03-12 19:07:58 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vladyslav Burdeniuk on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
