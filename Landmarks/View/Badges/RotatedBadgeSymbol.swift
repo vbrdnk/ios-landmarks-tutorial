4ba3e0a61b299f295e71dc41b8f1a49e9ae70a26
Vladyslav Burdeniuk <vladyslavburdeniuk@MrFreemsMachine.lan>
2024-03-14 13:57:54 -0700

    Add badges

---
+++ Landmarks/View/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Vladyslav Burdeniuk on 3/14/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
