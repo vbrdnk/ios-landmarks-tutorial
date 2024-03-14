c1af9aebabd7257b81d2af3c60c02583a415b0e5
Vladyslav Burdeniuk <vladyslavb@spotify.com>
2024-03-14 12:19:55 -0700

    Initial commit

---
+++ Landmarks/View/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Vladyslav Burdeniuk on 3/12/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
