7f75b1f1f016b177edf74c667929a39093068e76
Vladyslav Burdeniuk <vladyslavburdeniuk@MrFreemsMachine.lan>
2024-03-14 14:46:30 -0700

    Create tabs and navigation

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -12,6 +12,17 @@ import Foundation
 class ModelData {
     var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
+    
+    var featured: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
